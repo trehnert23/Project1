fe1837b46260bfdfd50ebbb6d6c96439ce282023
Thomas Rehnert <tomrehnert2011@gmail.com>
2015-09-08 15:57:27 -0700

    setting up loop to create targets

--- public/javascripts/profile.js
+++ public/javascripts/profile.js
@@ -1,5 +1,47 @@
 $(document).ready(function(){
-	
+
+	var counter = 0;
+
+
+	$('.shooting_gallery').append("<img class ='target' src='http://www.cliparthut.com/clip-arts/88/bullseye-target-88907.png'>");
+	$(".target").on("click",function(event) {
+  		$(this).remove();
+  		counter++;
+	});
+
+
+
+	// $(".target1").on("click",function(event) {
+ //  		$(this).remove();
+ //  		counter++;
+	// });
+
+	// $(".target2").on("click",function(event) {
+ //  		$(this).remove();
+ //  		counter++;
+	// });
+
+	// $(".target3").on("click",function(event) {
+ //  		$(this).remove();
+ //  		counter++;
+	// });
+
+	// $(".target4").on("click",function(event) {
+ //  		$(this).remove();
+ //  		counter++;
+	// });
+
+	// $(".target5").on("click",function(event) {
+ //  		$(this).remove();
+ //  		counter++;
+	// });
+
+	// $(".target6").on("click",function(event) {
+ //  		$(this).remove();
+ //  		counter++;
+	// });
+
+
 	// $(function shuffle() {
 	//     var parent = $("#shuffle");
 	//     var divs = parent.children();
@@ -13,50 +55,6 @@ $(document).ready(function(){
 	//     location.reload();
 	// })
 
-
-
-	var counter = 0;
-
-	$(".target1").on("click",function(event) {
-  		$(this).remove();
-  		counter++;
-	});
-
-	$(".target2").on("click",function(event) {
-  		$(this).remove();
-  		counter++;
-	});
-
-	$(".target3").on("click",function(event) {
-  		$(this).remove();
-  		counter++;
-	});
-
-	$(".target4").on("click",function(event) {
-  		$(this).remove();
-  		counter++;
-	});
-
-	$(".target5").on("click",function(event) {
-  		$(this).remove();
-  		counter++;
-	});
-
-	$(".target6").on("click",function(event) {
-  		$(this).remove();
-  		counter++;
-	});
-
-
-
-	var move = function() {
-	    $(".target").animate({"left": "-40px"}, 1000, function() { 
-	        $(this).animate({"left": "40px"}, 1000) 
-	    })
-	 setTimeout(move, 2000);
-	}
-
-	move();
 
 });
 

c12d7a7ea571dffcfa9906e413bdeebbfb27aecf
Thomas Rehnert <tomrehnert2011@gmail.com>
2015-09-08 15:11:58 -0700

    updated look and added targets to game

--- public/javascripts/profile.js
+++ public/javascripts/profile.js
@@ -1,7 +1,58 @@
 $(document).ready(function(){
+	
+	// $(function shuffle() {
+	//     var parent = $("#shuffle");
+	//     var divs = parent.children();
+	//     while (divs.length) {
+	//         parent.append(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
+	//     }
+	// })
+	// ​
 
-	$(".image1").on("click",function(event) {
-  		$(".image1").remove();
+	// $("#reset").click(function(){
+	//     location.reload();
+	// })
+
+
+
+	var counter = 0;
+
+	$(".target1").on("click",function(event) {
+  		$(this).remove();
+  		counter++;
+	});
+
+	$(".target2").on("click",function(event) {
+  		$(this).remove();
+  		counter++;
+	});
+
+	$(".target3").on("click",function(event) {
+  		$(this).remove();
+  		counter++;
+	});
+
+	$(".target4").on("click",function(event) {
+  		$(this).remove();
+  		counter++;
+	});
+
+	$(".target5").on("click",function(event) {
+  		$(this).remove();
+  		counter++;
+	});
+
+	$(".target6").on("click",function(event) {
+  		$(this).remove();
+  		counter++;
 	});
 
 });
+
+
+
+
+
+
+
+

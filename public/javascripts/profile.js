d19454817122d9aea9b0e482ed2a8e440c648981
Thomas Rehnert <tomrehnert2011@gmail.com>
2015-09-09 11:55:18 -0700

    finished game now working on scoreboard and timer

--- public/javascripts/profile.js
+++ public/javascripts/profile.js
@@ -3,6 +3,21 @@ $(document).ready(function(){
 	var counter = 0;
 	var score = 0;
 	var numTargs = 6;
+	var timer = 0;
+
+
+function createTarget() {
+	var sizeRand = Math.random()*60+40;
+	var leftMargRand = Math.random()*90;
+	var topMargRand = Math.random()*43;
+	$(".shooting_gallery").append("<img class='target"+numTargs+" targets' src='http://www.cliparthut.com/clip-arts/88/bullseye-target-88907.png'>");
+	$(".target"+numTargs).css("width", sizeRand+"px");
+	$(".target"+numTargs).css("height", sizeRand+"px");
+	$(".target"+numTargs).css("margin-left", leftMargRand+"%");
+	$(".target"+numTargs).css("margin-top", topMargRand+"%");
+	$(".target"+numTargs).css("position", "absolute");
+	numTargs--;
+}
 
 
 function Countdown(countdown) {
@@ -10,42 +25,80 @@ function Countdown(countdown) {
 		var seconds_left = countdown;
 		var interval = setInterval(function() {
 		    //make the targets visible && locked
-		   document.getElementById('timer_div').innerHTML = --seconds_left;
-		   if (seconds_left <= 0 ){
-		        clearInterval(interval);
-		      	//remove timer numbers
-		      	$('#timer_div').remove();
-		   
-			    function createTarget() {
-			    	var sizeRand = Math.random()*60+40;
-			    	var leftMargRand = Math.random()*90;
-			    	var topMargRand = Math.random()*43;
-			    	$(".shooting_gallery").append("<img class='target"+numTargs+"' id='target' src='http://www.cliparthut.com/clip-arts/88/bullseye-target-88907.png'>");
-			    	$(".target"+numTargs).css("width", sizeRand+"px");
-			    	$(".target"+numTargs).css("height", sizeRand+"px");
-			    	$(".target"+numTargs).css("margin-left", leftMargRand+"%");
-			    	$(".target"+numTargs).css("margin-top", topMargRand+"%");
-			    	$(".target"+numTargs).css("position", "absolute");
-			    	numTargs--;
-			    }
+		   document.getElementById('countdown_div').innerHTML = --seconds_left;
+		   if(seconds_left <= 0 ){
+		   		if(!$("#timer_div").hasClass('pauseTimer')){
+			   		var player_time = setInterval(function(){
+			   			document.getElementById('timer_div').innerHTML = ++timer;
+			   		}, 1000);
+			      	//remove timer numbers
+			      	$('#countdown_div').remove();
 
-			    //creates targets
-			    while(numTargs>0){
-			    	createTarget();
-			    }
+				    // creates initial targets
+				    while(numTargs>0){
+				    	createTarget();
+				    }
 
-			    //make targets clickable
-	    		$("#target").on("click",function(event) {
-	    	  		$(this).remove();
-	    	  		score++;
-	    	  		console.log(score);
-	    		});
-
+				    //delete targets when clicked
+				    $(".targets").on("click",function(event) {
+		    	  		$(this).remove();
+		    	  		score++;
+		    	  		if(score<6){
+		    	  			console.log(score);
+		    	  		}
+		    	  		else{
+		    	  			Round();
+		    	  		}
+		    	  		numTargs=6;
+		    		});
+		    		clearInterval(interval);
+		    	}
+		    	else {
+		    		console.log("Stopped Counting");
+		    	}
 		   }
 		}, 1000);
 	});
 }
-Countdown(6);
+Countdown(2);
+
+
+
+function Round(){
+	while(numTargs>0){
+		createTarget();
+	}
+    $(".targets").on("click",function(event) {
+  		$(this).remove();
+  		score++;
+  		console.log(score);
+  		if(score===6){
+  			$("#timer_div").addClass('pauseTimer');
+  			// numTargs=6;
+  			// console.log("numTargs= ", numTargs);
+  			// Round();
+  		}
+  		else if(score===12){
+  			console.log("numTargs= ", numTargs);
+  			numTargs=6;
+  			Round();
+  		}
+  		else if(score===18){
+  			console.log("numTargs= ", numTargs);
+  			numTargs=6;
+  			Round();
+  		}
+  		else if(score===24){
+  			// $("#timer_div").addClass('pauseTimer');
+  			alert("winner");
+  		}
+	});
+}
+
+
+
+
+
 
 
 });

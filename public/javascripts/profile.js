317ff85505100a669e3db7b7cda5d260b8738ac6
Thomas Rehnert <tomrehnert2011@gmail.com>
2015-09-08 18:39:10 -0700

    make targets show up after timer

--- public/javascripts/profile.js
+++ public/javascripts/profile.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 
 	var counter = 0;
 	var score = 0;
+	var numTargs = 6;
 
 
 function Countdown(countdown) {
@@ -11,95 +12,40 @@ function Countdown(countdown) {
 		    //make the targets visible && locked
 		   document.getElementById('timer_div').innerHTML = --seconds_left;
 		   if (seconds_left <= 0 ){
-		        //make the targets clickable at 0
 		        clearInterval(interval);
-		      	$(".target").on("click",function(event) {
-		        		$(this).remove();
-		        		score++;
-		        		console.log(score);
-		      	});
+		      	//remove timer numbers
 		      	$('#timer_div').remove();
+		   
+			    function createTarget() {
+			    	var sizeRand = Math.random()*60+40;
+			    	var leftMargRand = Math.random()*90;
+			    	var topMargRand = Math.random()*43;
+			    	$(".shooting_gallery").append("<img class='target"+numTargs+"' id='target' src='http://www.cliparthut.com/clip-arts/88/bullseye-target-88907.png'>");
+			    	$(".target"+numTargs).css("width", sizeRand+"px");
+			    	$(".target"+numTargs).css("height", sizeRand+"px");
+			    	$(".target"+numTargs).css("margin-left", leftMargRand+"%");
+			    	$(".target"+numTargs).css("margin-top", topMargRand+"%");
+			    	$(".target"+numTargs).css("position", "absolute");
+			    	numTargs--;
+			    }
+
+			    //creates targets
+			    while(numTargs>0){
+			    	createTarget();
+			    }
+
+			    //make targets clickable
+	    		$("#target").on("click",function(event) {
+	    	  		$(this).remove();
+	    	  		score++;
+	    	  		console.log(score);
+	    		});
+
 		   }
 		}, 1000);
 	});
 }
-
 Countdown(6);
-
-
-function createTarget() {
-	var sizeRand = Math.random()*60+40;
-	console.log(sizeRand);
-	var leftMargRand = Math.random()*90;
-		console.log(leftMargRand);
-
-	var topMargRand = Math.random()*43;
-		console.log(topMargRand);
-
-	$(".shooting_gallery").append("<img class='target' src='http://www.cliparthut.com/clip-arts/88/bullseye-target-88907.png'>");
-	$(".target").css("width", sizeRand+"px");
-	$(".target").css("height", sizeRand+"px");
-	$(".target").css("margin-left", leftMargRand+"%");
-	$(".target").css("margin-top", topMargRand+"%");
-	$(".target").css("position", "absolute");
-	
-}
-
-
-
-
-	// $(".target1").on("click",function(event) {
- //  		$(this).remove();
- //  		counter++;
-	// });
-
-	// $(".target2").on("click",function(event) {
- //  		$(this).remove();
- //  		counter++;
-	// });
-
-	// $(".target3").on("click",function(event) {
- //  		$(this).remove();
- //  		counter++;
-	// });
-
-	// $(".target4").on("click",function(event) {
- //  		$(this).remove();
- //  		counter++;
-	// });
-
-	// $(".target5").on("click",function(event) {
- //  		$(this).remove();
- //  		counter++;
-	// });
-
-	// $(".target6").on("click",function(event) {
- //  		$(this).remove();
- //  		counter++;
-	// });
-
-
-	// $(function shuffle() {
-	//     var parent = $("#shuffle");
-	//     var divs = parent.children();
-	//     while (divs.length) {
-	//         parent.append(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
-	//     }
-	// })
-	// ​
-
-	// $("#reset").click(function(){
-	//     location.reload();
-	// })
-
-createTarget();
-createTarget();
-createTarget();
-createTarget();
-createTarget();
-createTarget();
-
-
 
 
 });
